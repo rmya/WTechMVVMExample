2ca804f80cf067e307a3c50dd205617652ac027c
rmya <tanrumeysa93@gmail.com>
2021-08-29 01:43:23 +0300

    layouts ve klasör yapısı oluşturuldu

---
+++ app/src/main/java/com/example/wtech_mvvm_example/activity/GirisActivity.kt
@@ -0,0 +1,13 @@
+package com.example.wtech_mvvm_example.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.wtech_mvvm_example.R
+
+class GirisActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        (super.onCreate(savedInstanceState))
+        setContentView(R.layout.activity_giris)
+    }
+}
---
+++ app/src/main/java/com/example/wtech_mvvm_example/fragments/ana/ProfilFragment.kt
@@ -0,0 +1,25 @@
+package com.example.wtech_mvvm_example.fragments.ana
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.wtech_mvvm_example.R
+
+
+class ProfilFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profil, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
