881fd866df9fa0d9f5ba4cbc5b75a96ff3c8a512
rmya <tanrumeysa93@gmail.com>
2021-09-07 23:44:41 +0300

    splash added

--- app/src/main/java/com/example/wtech_mvvm_example/activity/SplashActivity.kt
+++ app/src/main/java/com/example/wtech_mvvm_example/activity/SplashActivity.kt
@@ -1,13 +1,30 @@
 package com.example.wtech_mvvm_example.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.wtech_mvvm_example.R
+import java.lang.Exception
 
 class SplashActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val background = object : Thread(){
+            override fun run() {
+                try {
+                    sleep(2500)
+                    val intent = Intent(this@SplashActivity, GirisActivity::class.java)
+                    startActivity(intent)
+                }catch (e : Exception){
+                    e.printStackTrace()
+                }
+            }
+        }
+
+        background.start()
+
     }
 }
